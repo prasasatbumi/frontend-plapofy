7ac9509aa3f55bbfa57e6687c70f3d9d26553fca
prasasatbumi <prasasatbumi@gmail.com>
2026-01-13 11:52:44 +0700

    Add ngrok-skip-browser-warning header

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -9,7 +9,8 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     if (token) {
         req = req.clone({
             setHeaders: {
-                Authorization: `Bearer ${token}`
+                Authorization: `Bearer ${token}`,
+                'ngrok-skip-browser-warning': 'true'
             }
         });
     }
